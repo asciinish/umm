3c268b5f2b949e8073118dcecc59baf83fb8772a
asciinish <76739124+asciinish@users.noreply.github.com>
2025-07-03 10:34:15 +0530

    Update functions.js

--- file/functions.js
+++ file/functions.js
@@ -55,6 +55,6 @@ function timeElapse(date){
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
-	var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒"; 
+	var result = "T <span class=\"digit\">" + days + "</span> D <span class=\"digit\">" + hours + "</span> H <span class=\"digit\">" + minutes + "</span> M <span class=\"digit\">" + seconds + "</span> S"; 
 	$("#clock").html(result);
 }
